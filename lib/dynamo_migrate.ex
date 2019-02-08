507384b454132b64977d5171d41a12b8746b08e4
larrywax <giuseppe.danna@prima.it>
2019-02-08 18:07:51 +0100

    first try

--- lib/dynamo_migrate.ex
+++ lib/dynamo_migrate.ex
@@ -9,16 +9,16 @@ defmodule DynamoMigrate do
   @old_fiscal_code_table Application.get_env(:dynamo_migrate, :old_fiscal_code_table)
 
   def run do
-    # Logger.info("Start plate number table import")
-    #
-    # @old_plate_number_table
-    # |> Dynamo.scan(limit: 100)
-    # |> ExAws.request!()
-    # |> Repo.parse_result(OldPlateNumber)
-    # |> Enum.map(&Repo.convert_record/1)
-    # |> Enum.map(&Repo.insert(Application.get_env(:dynamo_migrate, :new_plate_number_table), &1))
-    #
-    # Logger.info("Plate number table import completed!")
+    Logger.info("Start plate number table import")
+
+    @old_plate_number_table
+    |> Dynamo.scan(limit: 100)
+    |> ExAws.request!()
+    |> Repo.parse_result(OldPlateNumber)
+    |> Enum.map(&Repo.convert_record/1)
+    |> Enum.map(&Repo.insert(Application.get_env(:dynamo_migrate, :new_plate_number_table), &1))
+
+    Logger.info("Plate number table import completed!")
 
     Logger.info("Start fiscal code table import")
 
